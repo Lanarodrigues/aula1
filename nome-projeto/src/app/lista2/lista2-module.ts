9b22a05b50076a35610390f5e8c520fa533e6a44
Lanarodrigues <lana96042@gmail.com>
2026-09-18 01:35:41 +0000

    Atividade

---
+++ nome-projeto/src/app/lista2/lista2-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Ex1 } from './ex1/ex1';
+
+@NgModule({
+  declarations: [Ex1],
+  imports: [CommonModule],
+  exports:[Ex1]
+})
+export class Lista2Module {}
